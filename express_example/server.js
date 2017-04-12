a79314f572c8ff2a54577b2d4719bd304af4aed5
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-11 22:34:31 -0700

    new routes for reading query strings

--- express_example/server.js
+++ express_example/server.js
@@ -33,6 +33,32 @@ app.get("/api/tacos", function(rec, res) {
   res.json(tacos);
 });
 
+app.get("/greetings/:name", function (req, res)  {
+  res.send("Hi, " + req.params.name);
+});
+
+app.get("/pick-a-color/:color", function (req, res) {
+  res.send("You chose teh color: "  + req.params.color);
+});
+
+// app.get("/thank", function (req, res) {
+//   console.log(req.query);
+//   var name = req.query.name;
+//   res.send('Thank you, ' + name + "!");
+// });
+
+app.get("/thank", function (request, response) {
+  console.log(request.query);
+  var name = request.query.name;
+  response.send('Thank you, ' + name + '!');
+});
+
+app.get("/multiply", function (req, res) {
+  var x = req.query.x;
+  var y = req.query.y;
+  res.send(x*y + " is the result");
+});
+
 //  SERVER START
 app.listen(3000, function () {
   console.log("HTTP server listening at localhost:3000");
